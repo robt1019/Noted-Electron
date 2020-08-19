46c0d8bed3e8b6a96e28c20787010b23a11e43f7
Rob Taylor <robert.i.taylor88@gmail.com>
2020-08-19 13:51:25 +0100

    sort fonts

---
+++ views/app.process.js
@@ -0,0 +1,46 @@
+const electron = require("electron");
+
+const BrowserWindow = electron.BrowserWindow;
+
+let notesWindow = null;
+
+const createWindow = (storedNotes) => {
+  destroyWindow();
+
+  notesWindow = new BrowserWindow({
+    width: 400,
+    height: 600,
+    webPreferences: {
+      preload: `${__dirname}/../../preload.js`,
+      nodeIntegration: false,
+      enableRemoteModule: false,
+      contextIsolation: true,
+      worldSafeExecuteJavaScript: true,
+    },
+  });
+
+  notesWindow.loadURL(`file://${__dirname}/notes.html`).then(() => {
+    if (storedNotes) {
+      notesWindow.webContents.send("notes", storedNotes);
+    }
+  });
+};
+
+const destroyWindow = () => {
+  if (notesWindow) {
+    notesWindow.close();
+    notesWindow = null;
+  }
+};
+
+const setNotes = (notes) => {
+  if (notesWindow) {
+    notesWindow.webContents.send("notes", notes);
+  }
+};
+
+module.exports = {
+  createWindow,
+  destroyWindow,
+  setNotes,
+};
